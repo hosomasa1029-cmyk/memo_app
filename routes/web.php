8801c3cc9290f50b951e217f34eb022ce1b8851b
hosomasa1029 <hosomasa1029@gmail.com>
2025-08-31 14:18:54 +0900

    更新処理まで完成

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,13 @@ Route::get('/', function () {
 
 
 
- // 一覧ページ
- Volt::route('/memos', 'memos.index')->name('memos.index');
- // 具体的なパスを先に定義
- // 新規作成ページ
- Volt::route('/memos/create', 'memos.create')->name('memos.create');
- // 動的パラメータを含むルートは最後に定義
+// 一覧ページ
+Volt::route('/memos', 'memos.index')->name('memos.index');
+// 具体的なパスを先に定義
+// 新規作成ページ
+Volt::route('/memos/create', 'memos.create')->name('memos.create');
+// 動的パラメータを含むルートは最後に定義
 // 詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
+// 編集ページ
+Volt::route('/memos/{memo}/edit', 'memos.edit')->name('memos.edit');
